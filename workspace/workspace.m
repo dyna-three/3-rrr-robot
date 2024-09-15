716dad1f01b53e7b72b2fb9170780a04613f3b01
Utkaarsh Saha <62903411+Kishou-Arima@users.noreply.github.com>
2024-09-15 14:42:34 +0530

    Update workspace.m

--- workspace/workspace.m
+++ workspace/workspace.m
@@ -73,11 +73,3 @@ grid on
 
 % This function is used to plot any circle with position x and y, and
 % radius r
-function h = circleplot(x,y,r)
-    hold on
-    th = 0:0.01:6.2832;
-    xunit = r * cos(th) + x;
-    yunit = r * sin(th) + y;
-    h = plot(xunit, yunit,'r');
-    hold off
-end
